154c8a89a2b5af4c574c02deb019c3c3e43f2fad
Praneeth Suresh <praneeth.suresh.s@gmail.com>
2025-02-16 16:08:08 +0800

    Attempt at PartyBox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import audiosong from './alone-296348.mp3'
+
+const {MusicBeatScheduler} = require('.')
+
+//MusicBeatScheduler syncs any detected peak with the listened audio. It's useful to control some bulbs or any other effect
+const musicBeatScheduler = new MusicBeatScheduler(pos => {
+  console.log(`peak at ${pos}ms`) //do your effect here
+})
+
+musicBeatScheduler.start()
+
+function MusicPLayer() {
+  return (
+    <>
+      <h1>Here plays Alone</h1>
+
+      <audio src={audiosong} controls />
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MusicPLayer />
       </header>
     </div>
   );
 }
+
+
 
 export default App;
